0eea484e8c599e44eb6b99530367a45529e155cb
Alaxander Zlatkov <alexander.z.zlatkov@gmail.com>
2013-01-22 22:49:57 +0200

    completed task 12 and 13 to C#2 TextFiles

---
+++ TelerikC#2/TextFiles/CountWordsOccurrencesInFile/CountWordsOccurrencesInFile.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security;
+
+namespace CountWordsOccurrencesInFile
+{
+    class ComparePairs : IComparer<KeyValuePair<string, int>>
+    {
+        public int Compare(KeyValuePair<string, int> x, KeyValuePair<string, int> y)
+        {
+            if (x.Value > y.Value)
+            {
+                return -1;
+            }
+            else if (x.Value < y.Value)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    class CountWordsOccurrencesInFile
+    {
+        static List<string> ExtractWordsFromLine(string line)
+        {
+            List<string> words = new List<string>(); 
+            StringBuilder word = new StringBuilder();
+
+            foreach (char symbol in line)
+            {
+                if (symbol == ' ' && word.ToString() != String.Empty)
+                {
+                    words.Add(word.ToString());
+                    word.Clear();
+                }
+                else
+                {
+                    word.Append(symbol);
+                }
+            }
+
+            if (word.ToString() != String.Empty)
+            {
+                words.Add(word.ToString());
+            }
+
+            return words;
+        }
+        static List<string> ExtractWordsFromFile(string wordsFileName)
+        {
+            List<string> words = new List<string>();
+
+            using (StreamReader reader = new StreamReader(wordsFileName))
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    List<string> wordsOnLine = ExtractWordsFromLine(line);
+                    words.AddRange(wordsOnLine);
+
+                    line = reader.ReadLine();
+                }
+            }
+
+            return words;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<string> wordsToSearch = ExtractWordsFromFile("words.txt");
+                List<string> wordsInFile = ExtractWordsFromFile("test.txt");
+
+                List<KeyValuePair<string, int>> result = new List<KeyValuePair<string, int>>();
+
+                foreach (string wordToSearch in wordsToSearch)
+                {
+                    int occurrences = 0;
+                    foreach (string wordInFile in wordsInFile)
+                    {
+                        if (wordToSearch == wordInFile)
+                        {
+                            occurrences++;
+                        }
+                    }
+                    result.Add(new KeyValuePair<string, int>(wordToSearch, occurrences));
+                }
+
+                result.Sort(new ComparePairs());
+
+                using (StreamWriter writer = new StreamWriter("result.txt"))
+                {
+                    foreach (KeyValuePair<string, int> entry in result)
+                    {
+                        writer.WriteLine(entry.Key + " " + entry.Value);
+                    }
+                }
+            }
+            catch (OutOfMemoryException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (SecurityException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ TelerikC#2/TextFiles/RemoveWordsListedInFile/RemoveWordsListedInFile.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security;
+
+namespace RemoveWordsListedInFile
+{
+    class RemoveWordsListedInFile
+    {
+        static List<string> ExtractWordsFromLine(string line)
+        {
+            List<string> words = new List<string>();
+            StringBuilder word = new StringBuilder();
+
+            foreach (char symbol in line)
+            {
+                if (symbol == ' ' && word.ToString() != String.Empty)
+                {
+                    words.Add(word.ToString());
+                    word.Clear();
+                }
+                else
+                {
+                    word.Append(symbol);
+                }
+            }
+
+            if (word.ToString() != String.Empty)
+            {
+                words.Add(word.ToString());
+            }
+
+            return words;
+        }
+        static List<string> ExtractWordsFromFile(string wordsFileName)
+        {
+            List<string> words = new List<string>();
+
+            using (StreamReader reader = new StreamReader(wordsFileName))
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    List<string> wordsOnLine = ExtractWordsFromLine(line);
+                    words.AddRange(wordsOnLine);
+
+                    line = reader.ReadLine();
+                }
+            }
+
+            return words;
+        }
+
+        static void Main(string[] args)
+        {
+            string inputFileName = Console.ReadLine();
+            string wordsFileName = Console.ReadLine();
+
+            try
+            {
+                List<string> forbiddenWords = ExtractWordsFromFile(wordsFileName);
+                List<string> inputFileWords = new List<string>();
+
+                using (StreamReader reader = new StreamReader(inputFileName))
+                {
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        inputFileWords.AddRange(ExtractWordsFromLine(line));
+                        inputFileWords.Add("\n");
+                        line = reader.ReadLine();
+                    }
+                }
+
+                using (StreamWriter writer = new StreamWriter(inputFileName))
+                {
+                    foreach (string word in inputFileWords)
+                    {
+                        if (!forbiddenWords.Contains(word))
+                        {
+                            writer.Write(word);
+                            if (word != "\n")
+                            {
+                                writer.Write(" ");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (OutOfMemoryException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (SecurityException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
