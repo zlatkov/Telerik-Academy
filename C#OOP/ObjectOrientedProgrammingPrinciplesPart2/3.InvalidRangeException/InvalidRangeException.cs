a807d496cf58ef51ad6650e1005212c7e5add4cd
Alaxander Zlatkov <alexander.z.zlatkov@gmail.com>
2013-03-08 01:14:13 +0200

    OOP Principles part 2, task 3 - InvalidRangeException

---
+++ C#OOP/ObjectOrientedProgrammingPrinciplesPart2/3.InvalidRangeException/InvalidRangeException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.InvalidRangeException
+{
+    public class InvalidRangeException<T> : ApplicationException
+    {
+        public InvalidRangeException(string message, T start, T end)
+            : base(message)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+
+        public T Start { private set; get; }
+        public T End { private set; get; }
+    }
+}
---
+++ C#OOP/ObjectOrientedProgrammingPrinciplesPart2/3.InvalidRangeException/Tester.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.InvalidRangeException
+{
+    class Tester
+    {
+        static void Main(string[] args)
+        {
+            int startInt = int.Parse(Console.ReadLine());
+            int endInt = int.Parse(Console.ReadLine());
+
+            if (startInt > endInt)
+            {
+                throw new InvalidRangeException<int>("Int out of range.", startInt, endInt);
+            }
+
+            string startString = Console.ReadLine();
+            string endString = Console.ReadLine();
+
+            DateTime startDate = DateTime.Parse(startString);
+            DateTime endDate = DateTime.Parse(endString);
+
+            if (startDate.CompareTo(endDate) > 0)
+            {
+                throw new InvalidRangeException<DateTime>("DateTime out of range.", startDate, endDate);
+            }
+        }
+    }
+}
