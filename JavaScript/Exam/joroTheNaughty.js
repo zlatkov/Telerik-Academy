50dcb844b0a55576e76250ddb9c95eaf1efd282a
Alaxander Zlatkov <alexander.z.zlatkov@gmail.com>
2013-04-03 00:04:12 +0300

    sync joro the rabbit

--- JavaScript/Exam/joroTheNaughty.js
+++ JavaScript/Exam/joroTheNaughty.js
@@ -1,30 +1,29 @@
 function Solve(args) {
-    console.log('yes');
     var line0 = args[0].split(' ');
     var line1 = args[1].split(' ');
-
+ 
     var n = parseInt(line0[0]);
     var m = parseInt(line0[1]);
     var jumpsCount = parseInt(line0[2]);
-
+ 
     var currentX = parseInt(line1[0]) + 1;
     var currentY = parseInt(line1[1]) + 1;
-
+ 
     var jumps = [], i, j, tmp;
     for (i = 0; i < jumpsCount; ++i) {
         tmp = args[2 + i].split(' ');
         jumps.push([parseInt(tmp[0]), parseInt(tmp[1]) ]);
     }
-
+ 
     var visited = [];
     for (i = 0; i <= n; ++i) {
         visited[i] = [];
     }
-
+ 
     var nextX, nextY, score = 0, escaped = false, jumpIndex = 0;
-
+ 
     while (true) {
-
+ 
         if (currentX < 1 || currentX > n || currentY < 1 || currentY > m) {
             escaped = true;
             break;
@@ -33,13 +32,13 @@ function Solve(args) {
             escaped = false;
             break;
         }
-
+ 
         score += (currentX - 1) * m + currentY;
         currentX += jumps[jumpIndex][0];
         currentY += jumps[jumpIndex][1];
         jumpIndex = (jumpIndex + 1) % jumpsCount;
     }
-
+ 
     if (escaped) {
         return 'escaped ' + score;
     }
@@ -47,7 +46,6 @@ function Solve(args) {
         return 'caught ' + score;
     }
 }
-
 
 (function () {
 
