fcdb3db1db555f2f2a3ca4c7f6738056ca6021b5
Alaxander Zlatkov <alexander.z.zlatkov@gmail.com>
2013-05-30 01:22:36 +0300

    extra more

---
+++ HighQualityProgrammingCode/StructuresConditionalStatementsAndLoops/3.RefactoredLoop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.RefactoredLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool valueIsFound = false;
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(array[i]);
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine(array[i]);
+                    if (array[i] == expectedValue)
+                    {
+                        valueIsFound = true;
+                        break;
+                    }
+                }
+            }
+            if (valueIsFound)
+            {
+                Console.WriteLine("Value Found");
+            }
+
+        }
+    }
+}
