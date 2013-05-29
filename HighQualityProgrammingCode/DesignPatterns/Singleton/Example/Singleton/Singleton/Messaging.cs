864217bf7a7f2102e7d2beff9a8fce0d8549ff48
Alaxander Zlatkov <alexander.z.zlatkov@gmail.com>
2013-05-30 01:27:35 +0300

    more

---
+++ HighQualityProgrammingCode/DesignPatterns/Singleton/Example/Singleton/Singleton/Messaging.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Messaging
+    {
+        // A private instance of the class
+        private static Messaging instance;
+        private System.Collections.Generic.List<string> Messages;
+
+        // The constructor for this message is private, so that cannot be instantiated
+        private Messaging()
+        {
+            Messages = new System.Collections.Generic.List<string>();
+        }
+
+        // This private static property, will check if the static instance of the message
+        // is instantiated if it is then it will return the static instance, if not it will
+        // create a new instance.
+        public static Messaging Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Messaging();
+                return instance;
+            }
+        }
+
+        public void AddMessage(string Message)
+        {
+            Messages.Add(Message);
+        }
+
+        public bool HasMessage(string Message)
+        {
+            return Messages.Contains(Message);
+        }
+    }
+
+}
---
+++ HighQualityProgrammingCode/DesignPatterns/Singleton/Example/Singleton/Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Messaging.Instance.AddMessage("Testing Singleton.");
+
+            if (Messaging.Instance.HasMessage("Testing Singleton."))
+            {
+                Console.WriteLine("Singleton works!");
+            }
+            if (Messaging.Instance.HasMessage("Other Message."))
+            {
+                Console.WriteLine("Singleton does not work..");
+            }
+        }
+    }
+}
