5afeb852510fb4aa4e788e9163041133c7a9b32d
Alaxander Zlatkov <alexander.z.zlatkov@gmail.com>
2013-05-30 01:27:01 +0300

    more

---
+++ HighQualityProgrammingCode/DesignPatterns/AbstractFactory/Example/AbstractFactory/AbstractFactory/AbstractFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    /// <summary>
+    /// The 'AbstractFactory' abstract class
+    /// </summary>
+    public abstract class AbstractFactory
+    {
+        public abstract AbstractProductA CreateProductA();
+        public abstract AbstractProductB CreateProductB();
+    }
+}
